0a4e1d4e342303d478f580fa228b9d5a0390b213
jackiejing <jackie.jingg1125@gmail.com>
2022-06-29 15:29:00 -0400

    Create GussedNumber.cs

---
+++ GussedNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    public static class GussedNumber
+    {
+        public static void guessValue(string[] args)
+        {
+            Random random = new Random();
+            int rangeofValue = random.Next(1, 3);
+            int guessNumber = 0;
+            while (guessNumber != rangeofValue)
+            {
+                guessNumber = int.Parse(Console.ReadLine());
+                if (guessNumber > rangeofValue)
+                {
+                    Console.WriteLine("Too high.");
+                }
+                else if (guessNumber < rangeofValue)
+                {
+                    Console.WriteLine("Too low");
+                }
+            }
+            Console.WriteLine("Correct Answer");
+            Console.ReadLine();
+        }
+    }
+}
