c5d6f6dab2fb558f59cfac4e53f0ab37c856cf21
jackiejing <jackie.jingg1125@gmail.com>
2022-06-29 15:29:06 -0400

    Create Pyramid.cs

---
+++ Pyramid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    public static class Pyramid
+    {
+        public static void PyramidValue()
+        {
+            int i, j, k;
+            Console.WriteLine("Input of number: ");
+            //k = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < k; i++)
+            {
+                for (j = 1; j <= k; j++)
+                {
+                    Console.WriteLine(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
