c8731e9d9ad9ec88658777edb5eda246141f2ffd
jackiejing <jackie.jingg1125@gmail.com>
2022-06-29 15:29:03 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 1.
+/*
+Console.WriteLine($"sbyte: The bytes of mamory is {sizeof(sbyte)}, the MinValue is {sbyte.MinValue}, the MaxValue is {sbyte.MaxValue}");
+Console.WriteLine($"byte: The bytes of mamory is {sizeof(byte)}, the MinValue is {byte.MinValue}, the MaxValue is {byte.MaxValue}");
+Console.WriteLine($"short: The bytes of mamory is {sizeof(short)}, the MinValue is {short.MinValue}, the MaxValue is {short.MaxValue}");
+Console.WriteLine($"ushort: The bytes of mamory is {sizeof(ushort)}, the MinValue is {ushort.MinValue}, the MaxValue is {ushort.MaxValue}");
+Console.WriteLine($"int: The bytes of mamory is {sizeof(int)}, the MinValue is {int.MinValue}, the MaxValue is {int.MaxValue}");
+Console.WriteLine($"uint: The bytes of mamory is {sizeof(uint)}, the MinValue is {uint.MinValue}, the MaxValue is {uint.MaxValue}");
+Console.WriteLine($"long: The bytes of mamory is {sizeof(long)}, the MinValue is {long.MinValue}, the MaxValue is {long.MaxValue}");
+Console.WriteLine($"ulong: The bytes of mamory is {sizeof(ulong)}, the MinValue is {ulong.MinValue}, the MaxValue is {ulong.MaxValue}");
+Console.WriteLine($"float: The bytes of mamory is {sizeof(float)}, the MinValue is {float.MinValue}, the MaxValue is {float.MaxValue}");
+Console.WriteLine($"double: The bytes of mamory is {sizeof(double)}, the MinValue is {double.MinValue}, the MaxValue is {double.MaxValue}");
+Console.WriteLine($"decimal: The bytes of mamory is {sizeof(decimal)}, the MinValue is {decimal.MinValue}, the MaxValue is {decimal.MaxValue}");
+*/
+
+// 2.
+/*
+ulong centuries = ulong.Parse(Console.ReadLine());
+ulong years = centuries * 100;
+ulong days = years * 365;
+ulong hours = days * 24;
+ulong minutes = hours * 60;
+ulong seconds = minutes * 60;
+ulong milliseconds = seconds * 1000;
+ulong microsecends = milliseconds * 1000;
+ulong nanoseconds = microsecends * 1000;
+Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microsecends} microseconds = {nanoseconds} nanoseconds");
+*/
+/*
+int max = 500;
+for(byte i = 0; i <= max; i++)
+{
+    Console.WriteLine(i);
+}
+// it will print the number from 0 to 255 as a loop.
+*/
+
+
+
